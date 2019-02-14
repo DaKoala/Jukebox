054ceaf85c0abfbad790ec430230e4eb46701877
billyzou <yz3352@nyu.edu>
2019-02-14 16:40:13 -0500

    refactor: organize the code

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ class Song {
         this.name = name;
         this.audio = new Audio(sound);
     };
+
     play() {
         this.audio.play();
     };
 }
 
 class Jukebox {
-    constructor(songs, playId,nextId, previousId,stopId) {
+    constructor(songs, playId, nextId, previousId, stopId) {
         this.songs = songs;
         this.songPointer = 0;
         this.playBtn = document.getElementById(playId);
@@ -30,35 +31,58 @@ class Jukebox {
         });
     }
 
-    playCurrent() {
-        const song = this.songs[this.songPointer].audio;
-        if (song.paused) {
-            song.play();
-        } else {
-            song.pause();
-        }
-    }
-    stop(){
-      this.songs[this.songPointer].audio.pause();
-      this.songs[this.songPointer].audio.currentTime = 0;
-    }
-    next(){
-      this.songs[this.songPointer].audio.pause();
-      this.songs[this.songPointer].audio.currentTime = 0;
-      this.songPointer++;
-      this.songs[this.songPointer].audio.play();
-    }
-    previous(){
-      this.songs[this.songPointer].audio.pause();
-      this.songs[this.songPointer].audio.currentTime = 0;
-      this.songPointer--;
-      this.songs[this.songPointer].audio.play();
+    getCurrentSong() {
+        return this.songs[this.songPointer];
     }
 
+    getCurrentAudio() {
+        return this.getCurrentSong().audio();
+    }
+
+    playCurrent() {
+        const audio = this.getCurrentAudio();
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        return this;
+    }
+
+    stop() {
+        const audio = this.getCurrentAudio();
+        audio.pause();
+        audio.currentTime = 0;
+        return this;
+    }
+
+    next() {
+        this.stop();
+        this.increasePointer();
+        this.playCurrent();
+        return this;
+    }
+
+    previous() {
+        this.stop();
+        this.decreasePointer();
+        this.playCurrent();
+        return this;
+    }
+
+    increasePointer() {
+        const isOutOfBound = this.songPointer >= this.songs.length;
+        this.songPointer = isOutOfBound ? 0 : this.songPointer + 1;
+    }
+
+    decreasePointer() {
+        const isOutOfBound = this.songPointer <= 0;
+        this.songPointer = isOutOfBound ? this.songs.length - 1 : this.songPointer - 1;
+    }
 }
 
 const songs = [
     new Song('whatever', 'whatever.mp3'),
     new Song('heyjude', 'heyjude.mp3'),
 ];
-const jukebox = new Jukebox(songs, 'play','next','previous','stop');
+const jukebox = new Jukebox(songs, 'play', 'next', 'previous', 'stop');
