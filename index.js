40d108863452bbf222d09c79bd71b5f0ef3d9bf0
billyzou <yz3352@nyu.edu>
2019-02-14 15:22:19 -0500

    merge codes

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-function Song(){
-  this.name = " ";
-  this.sound = " ";//audio file
-  this.constructor = function (name, sound){
-    this.name = name;
-    this.sound = sound;
-  };
-  this.Play() = function(){
-    var audio = new Audio(this.sound);
-    audio.play();
-  };
+class Song {
+    constructor(name, sound) {
+        this.name = name;
+        this.sound = sound;
+    };
+    play() {
+        const audio = new Audio(this.sound);
+        audio.play();
+    };
 }
+
+class Jukebox {
+    constructor(songs) {
+        this.songs = songs;
+    }
+}
+
+
