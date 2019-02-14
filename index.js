420f9e20a4c85c51bf022bc5c7c1165b64af525b
dogukanavci <37452679+dogukanavci@users.noreply.github.com>
2019-02-14 15:19:16 -0500

    Object created
    Added an mp3 file and created song class

---
+++ index.js
@@ -0,0 +1,12 @@
+function Song(){
+  this.name = " ";
+  this.sound = " ";//audio file
+  this.constructor = function (name, sound){
+    this.name = name;
+    this.sound = sound;
+  };
+  this.Play() = function(){
+    var audio = new Audio(this.sound);
+    audio.play();
+  };
+}
