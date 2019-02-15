9b25662f18bb14c8413a34bb810236c75d0814bf
billyzou <yz3352@nyu.edu>
2019-02-14 23:03:12 -0500

    feat: track song progress

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 class Song {
-    constructor(name, sound) {
+    constructor(name, artist) {
         this.name = name;
-        this.audio = new Audio(sound);
+        this.audio = new Audio(`audio/${name}.mp3`);
+        this.artist = artist;
+        this.cover = `cover/${name}.JPG`;
     };
 
     play() {
@@ -10,36 +12,70 @@ class Song {
 }
 
 class Jukebox {
-    constructor(songs, playId, nextId, previousId, stopId, playIconId) {
-        this.songs = songs;
+    static secondToTimer(second) {
+        const sec = Math.floor(second % 60);
+        const min = Math.floor(second / 60);
+        const secStr = sec >= 10 ? String(sec) : `0${sec}`;
+        const minStr = min >= 10 ? String(min) : `0${min}`;
+        return `${minStr}:${secStr}`;
+    }
+
+    constructor(params) {
+        this.songs = params.songs;
+        this.songs.forEach(({ audio }) => {
+            audio.addEventListener('ended', () => {
+                this.next();
+            });
+        });
         this.songPointer = 0;
-        this.playIconId = playIconId;
-        this.playBtn = document.getElementById(playId);
-        this.playBtn.addEventListener('click', () => {
+        this.rotateDeg = 0;
+        this.dom = {};
+        this.dom.title = document.getElementById(params.titleId);
+        this.dom.playIcon = document.getElementById(params.playIconId);
+        this.dom.cover = document.getElementById(params.coverId);
+        this.dom.progress = document.getElementById(params.progressId);
+        this.dom.timer = document.getElementById(params.timerId);
+        this.dom.playBtn = document.getElementById(params.playId);
+        this.dom.playBtn.addEventListener('click', () => {
             this.playCurrent();
         });
-        this.nextBtn = document.getElementById(nextId);
-        this.nextBtn.addEventListener('click', () => {
+        this.dom.nextBtn = document.getElementById(params.nextId);
+        this.dom.nextBtn.addEventListener('click', () => {
             this.next();
         });
-        this.previousBtn = document.getElementById(previousId);
-        this.previousBtn.addEventListener('click', () => {
+        this.dom.previousBtn = document.getElementById(params.previousId);
+        this.dom.previousBtn.addEventListener('click', () => {
             this.previous();
         });
-        this.stopBtn = document.getElementById(stopId);
-        this.stopBtn.addEventListener('click', () => {
+        this.dom.stopBtn = document.getElementById(params.stopId);
+        this.dom.stopBtn.addEventListener('click', () => {
             this.stop();
+        });
+        this.getCurrentAudio().addEventListener('loadeddata', () => {
+            this.updateInfo();
+            this.rotateCover();
+        });
+        this.getCurrentAudio().addEventListener('timeupdate', () => {
+            this.updateProgress()
         });
     }
 
+    rotateCover() {
+        const cover = this.dom.cover;
+        const audio = this.getCurrentAudio();
+        this.rotateDeg = audio.paused ? this.rotateDeg : (this.rotateDeg + 1) % 360;
+        cover.style.transform = `rotate(${this.rotateDeg}deg)`;
+        setTimeout(this.rotateCover.bind(this), 20);
+    }
+
     changeIconToPlay() {
-        const icon = document.getElementById(this.playIconId);
+        const icon = this.dom.playIcon;
         icon.classList.remove('fa-pause');
         icon.classList.add('fa-play');
     }
 
     changeIconToPause() {
-        const icon = document.getElementById(this.playIconId);
+        const icon = this.dom.playIcon;
         icon.classList.remove('fa-play');
         icon.classList.add('fa-pause');
     }
@@ -50,6 +86,20 @@ class Jukebox {
 
     getCurrentAudio() {
         return this.getCurrentSong().audio;
+    }
+
+    updateInfo() {
+        const song = this.getCurrentSong();
+        this.dom.title.textContent = `${song.artist} - ${song.name}`;
+        this.dom.cover.style.backgroundImage = `url('${song.cover}')`;
+        this.updateProgress();
+    }
+
+    updateProgress() {
+        const audio = this.getCurrentAudio();
+        const { currentTime, duration } = audio;
+        this.dom.progress.style.width = `${currentTime / duration * 100}%`;
+        this.dom.timer.textContent = `${Jukebox.secondToTimer(currentTime)}/${Jukebox.secondToTimer(duration)}`;
     }
 
     playCurrent() {
@@ -64,6 +114,17 @@ class Jukebox {
         return this;
     }
 
+    changeSong(pointerFunc=(function(){})) {
+        this.stop();
+        pointerFunc();
+        this.playCurrent();
+        this.changeIconToPause();
+        this.updateInfo();
+        this.getCurrentAudio().addEventListener('timeupdate', () => {
+            this.updateProgress()
+        });
+    }
+
     stop() {
         const audio = this.getCurrentAudio();
         audio.pause();
@@ -73,23 +134,17 @@ class Jukebox {
     }
 
     next() {
-        this.stop();
-        this.increasePointer();
-        this.playCurrent();
-        this.changeIconToPause();
+        this.changeSong(this.increasePointer.bind(this));
         return this;
     }
 
     previous() {
-        this.stop();
-        this.decreasePointer();
-        this.playCurrent();
-        this.changeIconToPause();
+        this.changeSong(this.decreasePointer.bind(this));
         return this;
     }
 
     increasePointer() {
-        const isOutOfBound = this.songPointer >= this.songs.length;
+        const isOutOfBound = this.songPointer >= this.songs.length - 1;
         this.songPointer = isOutOfBound ? 0 : this.songPointer + 1;
     }
 
@@ -100,7 +155,20 @@ class Jukebox {
 }
 
 const songs = [
-    new Song('whatever', 'whatever.mp3'),
-    new Song('heyjude', 'heyjude.mp3'),
+    new Song('Believe', 'Dima Bilan'),
+    new Song('Just A Dream', 'Drew Ryniewicz'),
+    new Song('Just Cant Get Enough', 'Black Eyed Peas'),
+    new Song('Letting Go', 'Jo De La Rosa'),
 ];
-const jukebox = new Jukebox(songs, 'play', 'next', 'previous', 'stop', 'play-icon');
+const jukebox = new Jukebox({
+    songs,
+    playId: 'play',
+    nextId: 'next',
+    previousId: 'previous',
+    stopId: 'stop',
+    playIconId: 'play-icon',
+    titleId: 'title',
+    coverId: 'cover',
+    progressId: 'progress',
+    timerId: 'timer',
+});
